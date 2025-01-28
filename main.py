4859ed347167a054473f5711823e7e396650c79e
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-01-28 21:58:00 +0300

    2801

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import utils
+from api_library import Book, Library
 
-def main():
-    utils.print_name('Alex')
-    utils.bye()
 
-main()
+library = Library()
 
+book_1 = Book(title="Капитанская дочка",
+              author="А.С.Пушкин",
+              year=1836,
+              genre="Роман")
+
+book_2 = Book(title="Герой нашего времени",
+              author="М.Ю.Лермонтов",
+              year=1836,
+              genre="Роман")
+
+library.add_book(book_1)
+library.add_book(book_2)
+
+books = library.get_books()
+for id_, book in books.items():
+    print(book.get_info())
---
+++ main_utils.py
@@ -0,0 +1,8 @@
+import utils
+
+def main():
+    utils.print_name('Alex')
+    utils.bye()
+
+main()
+
